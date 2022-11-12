a697da3d516d51b4ce344c4ce183e77693c97cf3
RenaanRabelo <112179397+RenaanRabelo@users.noreply.github.com>
2022-11-12 19:53:13 -0300

    Delete Lista 02 directory

--- Lista 02/script3.js
+++
@@ -1,9 +0,0 @@
-//Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste.
-//Calcular e escrever o valor do novo salário. 
-
-let salario = parseFloat(prompt("Informe o salário mensal do funcionário: "));
-let percentual = parseFloat(prompt("Informe o percentual de reajuste: "));
-
-let reajuste = salario + (salario * percentual / 100);
-
-document.write("O salário era de R$" + salario.toFixed(2) + ', com o reajuste de ' + percentual + '% passa a ser R$' + reajuste.toFixed(2));
